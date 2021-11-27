3b3a141e69816915bafb68fa90a2eb2396b47b73
cozzyde <54963496+cozheyuanzhangde@users.noreply.github.com>
2021-11-27 17:30:31 -0500

    menu-update

--- Assets/Scripts/InGameMenuCtrl.cs
+++ Assets/Scripts/InGameMenuCtrl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InGameMenuCtrl : MonoBehaviour
 {
@@ -10,5 +11,10 @@ public class InGameMenuCtrl : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         gameObject.SetActive(false);
+    }
+
+    public void ClickMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
